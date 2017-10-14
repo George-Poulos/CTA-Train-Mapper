0a80b2909033dc288e8a268f02db6ce8d1c8fe91
ConditionalCoder <Georgepoulos5@gmail.com>
2017-10-14 00:24:56 -0500

    First Commit

---
+++ Services/GetCTAJSON.js
@@ -0,0 +1,27 @@
+/**
+ * Created by George on 10/11/17.
+ */
+
+const request = require('request');
+const aws = require('aws-sdk');
+
+let s3 = new aws.S3({
+    apiKey: process.env.apiKey,
+    URL: process.env.URL
+});
+
+let GetCTAJSON = function (response, trainLine){
+    let URL = s3.URL + "?key=" + s3.apiKey + "&rt=" + trainLine + "&outputType=" + "JSON";
+    request(URL, { json: true }, (err, res, body) => {
+        if (err) {
+            console.log("Request for Json Failed!");
+            return console.log(err);
+        }
+        else{
+            let ret = body.ctatt.route[0].train;
+            response.json(ret);
+        }
+    });
+};
+
+module.exports = GetCTAJSON;
---
+++ public/javascripts/map.js
@@ -0,0 +1,109 @@
+'use strict';
+let map;
+let bounds;
+
+
+let currentOpenWindow = null;
+let gMarkers = [];
+let blue;
+let red;
+let brown;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: new google.maps.LatLng(41.95793, -87.73813),
+        mapTypeId: 'roadmap'
+    });
+
+    bounds  = new google.maps.LatLngBounds();
+    Promise.resolve('success').resolve(getBlueLine());
+}
+
+
+function getBlueLine(){
+        $.ajax({
+            type: "POST",
+            dataType: 'json',
+            url: "/getBlueLine",
+            contentType: "application/json; charset=utf-8",
+            success: function (msg) {
+                console.log(msg);
+                blue = msg;
+                addMarkersToMap(blue, '/images/blueLine.png');
+            }
+        });
+        Promise.resolve('success').then(getBrownLine());
+}
+
+function getBrownLine(){
+        $.ajax({
+            type: "POST",
+            dataType: 'json',
+            url: "/getBrownLine",
+            contentType: "application/json; charset=utf-8",
+            success: function (msg) {
+                console.log(msg);
+                brown = msg;
+                addMarkersToMap(brown, '/images/brownLine.png');
+            }
+        });
+        Promise.resolve('success').then(getRedLine());
+}
+
+function getRedLine(){
+        $.ajax({
+            type: "POST",
+            dataType: 'json',
+            url: "/getRedLine",
+            contentType: "application/json; charset=utf-8",
+            success: function (msg) {
+                console.log(msg);
+                red = msg;
+                addMarkersToMap(red, '/images/redLine.png');
+            }
+        });
+        Promise.resolve('Success');
+}
+
+function addMarkersToMap(blue, markerIcon){
+    let markers = [];
+    for (let i = 0; i < blue.length; i++){
+        let lat = parseFloat(blue[i].lat);
+        let lon = parseFloat(blue[i].lon);
+        let type = blue[i].nextStaNm;
+        let tmp = new google.maps.LatLng(lat, lon);
+        bounds.extend(tmp);
+        markers.push({
+            position: tmp,
+            type: type,
+            nextStaNm: blue[i].nextStaNm,
+            arrT: blue[i].arrT,
+            destNm: blue[i].destNm
+        });
+    }
+
+    // Create markers.
+    markers.forEach(function(feature) {
+        let marker = new google.maps.Marker({
+            position: feature.position,
+            icon: markerIcon,
+            map: map
+        });
+        gMarkers.push(marker);
+        let infoWindow = new google.maps.InfoWindow({
+            content : "<p>Next Stop : " + feature.nextStaNm + "</p><p>Direction : " + feature.destNm + "</p><p> Arrival Time : " + feature.arrT.substr(11) + "</p>"
+        });
+        marker.addListener('click', function() {
+
+            if(currentOpenWindow !== null) {
+                currentOpenWindow.close();
+            }
+
+            infoWindow.open(map, marker);
+            currentOpenWindow = infoWindow;
+        });
+    });
+    map.fitBounds(bounds);
+    map.panToBounds(bounds);
+}
