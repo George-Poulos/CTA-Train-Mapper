75860ca7ac3a795b8b02022f2cabfe91310295d1
ConditionalCoder <Georgepoulos5@gmail.com>
2017-10-14 00:46:41 -0500

    Attempt to fix Heroku config vars

--- app.js
+++ app.js
@@ -7,22 +7,18 @@ let bodyParser = require('body-parser');
 
 let index = require('./routes/index');
 let users = require('./routes/users');
-const aws = require('aws-sdk');
-
 
 let getTrainJSON = require('./Services/GetCTAJSON');
-global.s3 = new aws.S3({
-    apiKey: process.env.apiKey,
-    URL: process.env.URL
-});
 
 let app = express();
 
-// view engine setup
+global.apiKey = process.env.apiKey;
+global.URL = process.env.URL;
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -33,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/getBlueLine', function(req,res){
-  getTrainJSON(res,"blue");
+    getTrainJSON(res,"blue");
 });
 app.use('/getRedLine', function(req,res){
     getTrainJSON(res,"red");
@@ -50,13 +46,10 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
